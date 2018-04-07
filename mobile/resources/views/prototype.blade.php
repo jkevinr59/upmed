25ab5ac1564184f49b6b7c2dfa689600eaafadf9
Joshua Kevin Rachmadi <jokevin.rachmadi@gmail.com>
2018-04-07 17:47:52 +0700

    penghapusan mobile version, karena mau didevelop pribadi

--- mobile/resources/views/prototype.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.dashboard')
-@section('content')
-	<script type="text/javascript">
-	$(document).ready(function(){
-		$('#nav-track').addClass('selected');
-	});		
-	</script>
-@endsection

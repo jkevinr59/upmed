f27d2e5c33b965022caa7b878fb8d189c45316c0
MAULA IQBAL <rifqimaula@gmail.com>
2018-06-05 01:04:35 +0700

    5 juni

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -10,6 +10,9 @@ use Illuminate\Support\Facades\Input;
 use Validator;
 use Illuminate\Support\Facades\Auth;
 use App\Subjek;
+use App\Analisa;
+use App\Kondisi;
+use App\SmartMessage;
 class ServiceController extends Controller
 {
  public function show_view()
@@ -19,4 +22,21 @@ class ServiceController extends Controller
 		$data['user'] = Auth::user();
  	return view('service2',$data);
  }   
+ public function listAnalisaKesehatan()
+ {
+	 $query=Analisa::all();
+	 for ($i=0; $i < sizeof($query) ; $i++) { 
+		$subjek_1=Subjek::where('id','=',$query[$i]->subjek_1)->get();
+		$subjek_2=Subjek::where('id','=',$query[$i]->subjek_2)->get();
+		$subjek_3=Subjek::where('id','=',$query[$i]->subjek_3)->get();
+		$kondisi=Kondisi::where('id','=',$query[$i]->id_kondisi)->get();
+		$pesan=SmartMessage::where('id','=',$query[$i]->kode_pesan)->get();
+
+		$query[$i]->subjek_1_isi=$subjek_1[0]->Name;
+		$query[$i]->subjek_2_isi=$subjek_2[0]->Name;
+		$query[$i]->subjek_3_isi=$subjek_3[0]->Name;
+		$query[$i]->kondisi_isi=$kondisi[0]->deskripsi;
+		$query[$i]->pesan_isi=$pesan[0]->message;
+	 }
+ }
 }
